6b3e99be99e8ed08e19824caaa2c97e70ab14b74
Ali Parvizi <snparvizi75@gmial.com>
2021-08-05 17:30:04 +0430

    update models

--- music/models.py
+++ music/models.py
@@ -1,22 +1,40 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.urls import reverse
-# from django.db.models.fields import CharField
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+def song_icon_upload_location(instance, filename):
+    return "artists/%d/song/%s/icon_%s" % (instance.artist.full_name, instance.song_name, filename)
+
+def song_cover_upload_location(instance, filename):
+    return "artists/%d/song/%s/cover_%s" % (instance.artist.full_name, instance.song_name, filename)
 
 class Artist(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=150)
+    django_user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_profile", blank=True,
+                                       null=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
+    @property
+    def full_name(self):
+        return self.first_name + ' ' + self.last_name
+
     def __str__(self):
-        return self.name
+        return self.full_name
+
 
 # Create your models here.
-class Song(models.Model):   
-    id = models.AutoField(primary_key=True)
+class Song(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     song_name = models.CharField(max_length=300)
-    song_file = models.FileField(upload_to= 'songs')
-
-    def get_absolute_url(self):
-        return reverse("")
+    song_file = models.FileField(upload_to='songs')
+    icon = models.ImageField(upload_to=song_icon_upload_location,
+                             null=True,
+                             blank=True)
+    cover = models.ImageField(upload_to=song_cover_upload_location,
+                             null=True,
+                             blank=True)
 
     def __str__(self):
         return self.song_name + ' - ' + str(self.artist)
--- music/serilalizers.py
+++ music/serilalizers.py
@@ -4,4 +4,4 @@ from .models import Song
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = ['song_name', 'artist', 'song_file']
+        fields = ['pk', 'song_name', 'artist', 'song_file']
