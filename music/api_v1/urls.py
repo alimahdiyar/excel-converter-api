bcf9aeb23d78f067846c06df0a426623ca74650b
Ali Mahdiyar <alimahdiyar77@gmail.com>
2021-08-09 17:24:10 +0430

    add swagger

--- music/api_v1/urls.py
+++ music/api_v1/urls.py
@@ -1,20 +1,22 @@
+from django.conf.urls import url
 from django.urls import path
-from rest_framework.schemas import get_schema_view
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from music.api_v1.views import (
     SongListView, SongDetailView, ProducerListView, ArtistListView, SearchFilterView
 )
 
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="CafePay API",
-#         default_version='development version',
-#         description="CafePay API Documentation\n@spsina\n@SadafAsad",
-#         contact=openapi.Contact(email="snparvizi75@gmail.com"),
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-# )
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Station 49 API",
+        default_version='development version',
+        description="Station 49 API Documentation",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 app_name = 'Songs'
 urlpatterns = [
@@ -25,9 +27,9 @@ urlpatterns = [
     path('search/', SearchFilterView.as_view(), name='Song-list'),
 
         # docs
-    # url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 # urlpatterns += router.urls
